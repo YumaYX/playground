344325ecc072dd9732c5e3076b0525503e87cb2e
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-12-15 19:21:49 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use std::{env, fs, process, io};
+
 fn main() {
-    for n in 1..10000000 {
-        if n == 9 {
-            println!("z");
-        }
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file name>", args[0]);
+        process::exit(1);
     }
+
+    let file_name: &str = &args[1];
+    if let Err(e) = copy_file(file_name) {
+        eprintln!("Error copying file: {}", e);
+        process::exit(1);
+    }
+
+    println!("File copied successfully: {}", file_name);
 }
+
+fn copy_file(original: &str) -> io::Result<()> {
+    let bak = format!("{}.bak", original);
+    fs::copy(original, &bak)?;
+    Ok(())
+}
+
